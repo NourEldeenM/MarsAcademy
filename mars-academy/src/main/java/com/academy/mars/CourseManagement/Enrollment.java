a8d19783f3a518629bca74f65926362db0627b66
Omar-Mahmoud-25 <omar.mahmoud25102004@gmail.com>
2024-12-11 11:40:08 +0200

    added enrollment

---
+++ mars-academy/src/main/java/com/academy/mars/CourseManagement/Enrollment.java
@@ -0,0 +1,10 @@
+package com.academy.mars.CourseManagement;
+
+import com.academy.mars.UserManagement.Student;
+
+public class Enrollment {
+    private String ID;
+    private Course course;
+    private Student student;
+    private String status;
+}
