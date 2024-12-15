5dbeb5105649da47cf8105c3eae8fab148460772
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2024-12-15 15:53:53 +0200

    ..

--- mars-academy/src/main/java/com/academy/mars/MarsAcademyApplication.java
+++ mars-academy/src/main/java/com/academy/mars/MarsAcademyApplication.java
@@ -1,4 +1,5 @@
 package com.academy.mars;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarsAcademyApplication {
 
 	public static void main(String[] args) {
+//		Dotenv dotenv = Dotenv.configure().load();
+//		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(MarsAcademyApplication.class, args);
 	}
 
