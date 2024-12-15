00251083f4ced403d463ff98693acd3ecd0c2f83
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2024-12-15 20:39:38 +0200

    add authentication files

---
+++ mars-academy/src/main/java/com/academy/mars/UserManagement/UserRepository.java
@@ -0,0 +1,9 @@
+package com.academy.mars.UserManagement;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
