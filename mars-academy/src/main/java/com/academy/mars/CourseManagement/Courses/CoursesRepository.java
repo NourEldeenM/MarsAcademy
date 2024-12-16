551802b36d7985c3f3406acdd3447bb60fbc5785
Moaz-Esmail <moazmohamed.ba@gmail.com>
2024-12-16 20:59:38 +0200

    Feat: Courses Repository

--- mars-academy/src/main/java/com/academy/mars/CourseManagement/Courses/CoursesRepository.java
+++ mars-academy/src/main/java/com/academy/mars/CourseManagement/Courses/CoursesRepository.java
@@ -1,4 +1,18 @@
 package com.academy.mars.CourseManagement.Courses;
 
-public class CoursesRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CoursesRepository extends JpaRepository<Courses,String> {
+    void deleteByName(String courseName);
+
+    @Query("SELECT c FROM Courses c WHERE LOWER(c.name) = LOWER(:name)")
+    Optional<Courses> findByName(String name);
+    @Query("SELECT c FROM Courses c WHERE LOWER(c.category) = LOWER(:category)")
+    List<Courses> findByCategory(String category);
+
+
 }
