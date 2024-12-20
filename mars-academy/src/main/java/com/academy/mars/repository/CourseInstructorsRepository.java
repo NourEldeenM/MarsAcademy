1159f85b743e01eda26ff7d8e84b1489c3badc82
Moaz-Esmail <moazmohamed.ba@gmail.com>
2024-12-21 01:53:42 +0200

    Fix: course instructors table based on instructors table

--- mars-academy/src/main/java/com/academy/mars/repository/CourseInstructorsRepository.java
+++ mars-academy/src/main/java/com/academy/mars/repository/CourseInstructorsRepository.java
@@ -2,7 +2,7 @@ package com.academy.mars.repository;
 
 import com.academy.mars.entity.CourseInstructors;
 import com.academy.mars.entity.Courses;
-import com.academy.mars.entity.User;
+import com.academy.mars.entity.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,14 +16,14 @@ public interface CourseInstructorsRepository extends JpaRepository<CourseInstruc
     List<CourseInstructors> findByCourse(Courses course);
 
     // Find all courses for a given instructor
-    List<CourseInstructors> findByInstructor(User instructor);
+    List<CourseInstructors> findByInstructor(Instructor instructor);
 
     // Find specific CourseInstructor
-    Optional<CourseInstructors> findByCourseAndInstructor(Courses course, User instructor);
+    Optional<CourseInstructors> findByCourseAndInstructor(Courses course, Instructor instructor);
 
     // Delete all instructors for a given course
     void deleteByCourse(Courses course);
 
     // Delete specific instructor from a course
-    void deleteByCourseAndInstructor(Courses course, User instructor);
+    void deleteByCourseAndInstructor(Courses course, Instructor instructor);
 }
