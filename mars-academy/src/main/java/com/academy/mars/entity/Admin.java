bf45c2daff5124b3137848060379b61714717e7d
Moaz-Esmail <moazmohamed.ba@gmail.com>
2024-12-22 02:45:02 +0200

    Fix(user manangement): add admin class to complete business logic

--- mars-academy/src/main/java/com/academy/mars/entity/Admin.java
+++ mars-academy/src/main/java/com/academy/mars/entity/Admin.java
@@ -1,4 +1,23 @@
-//package com.academy.mars.entity;
-//
-//public class Admin {
-//}
+package com.academy.mars.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "admin")
+public class Admin {
+
+    @Id
+    private Long id; // Use this to map to User's ID
+
+    @OneToOne
+    @MapsId // Maps the ID of this entity to the ID of the related User entity
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+}
