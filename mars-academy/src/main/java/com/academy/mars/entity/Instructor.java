f6109a17e8621105c7819b2578dbc974a7f6c86f
Moaz-Esmail <moazmohamed.ba@gmail.com>
2024-12-22 03:12:59 +0200

    Feat: enable cascading delete for course instructors

--- mars-academy/src/main/java/com/academy/mars/entity/Instructor.java
+++ mars-academy/src/main/java/com/academy/mars/entity/Instructor.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -23,5 +25,8 @@ public class Instructor {
     @MapsId // Maps the ID of this entity to the ID of the related User entity
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
+
+    @OneToMany(mappedBy = "instructor",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<CourseInstructors> courses;
 
 }
