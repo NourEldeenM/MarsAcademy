2e640c60a3510fbb8b71fad26b0a8a21500c23f2
ebaa <ebaaaboelnaga@gmail.com>
2024-12-23 04:33:37 +0200

    initially user tests (kolo mwgod bs fady)

---
+++ mars-academy/src/test/java/com/academy/mars/repository/UserRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.academy.mars.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void deleteInBatch() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
---
+++ mars-academy/src/test/java/com/academy/mars/service/UserServiceTest.java
@@ -0,0 +1,45 @@
+package com.academy.mars.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void getUserByEmail() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void addNotification() {
+    }
+
+    @Test
+    void loadUserByUsername() {
+    }
+}
